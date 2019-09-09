4858c16a2059398d9def76585f5972b63fb0d672
Murilo Felpeto <murilogf@fastshop.com.br>
2019-09-09 15:28:12 -0300

    add comments for explanation

--- interpreter-pattern/src/main/java/br/com/murilo/allura/interpreter_pattern/App.java
+++ interpreter-pattern/src/main/java/br/com/murilo/allura/interpreter_pattern/App.java
@@ -10,11 +10,14 @@ import br.com.murilo.allura.interpreter_pattern.calculadora.Subtracao;
 public class App {
 	public static void main(String[] args) {
 
+		//(30 - 5) + (2 * 4) = 25 + 8 = 33
 		Expressao esquerda = new Soma(new Subtracao(new Numero(30), new Numero(5)),
 				new Multiplicacao(new Numero(2), new Numero(4)));
 
+		//10 - (8 / 2) = 10 - 4 = 6 
 		Expressao direita = new Subtracao(new Numero(10), new Divisao(new Numero(8), new Numero(2)));
 
+		// 33 + 6 = 39
 		Expressao conta = new Soma(esquerda, direita);
 		int resultado = conta.avalia();
 		System.out.println(resultado);
